cc41fb1505f89b6b83d87f9f096586bfa381ef3f
Datodarchiashvili123 <datodarchiashvili@gmail.com>
2024-10-28 18:57:23 +0400

    add news

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,11 @@ export const routes: Routes = [
       import('./pages/games/games.component').then((m) => m.GamesComponent),
   },
   {
+    path: 'news',
+    loadComponent: () =>
+        import('./pages/news/news.component').then((m) => m.NewsComponent),
+  },
+  {
     path: 'games/:id',
     loadComponent: () =>
         import('./pages/game-details/game-details.component').then((m) => m.GameDetailsComponent),

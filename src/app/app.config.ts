391eaf75891098d6bccde3b3094cf1a0e4117d8e
Datodarchiashvili123 <datodarchiashvili@gmail.com>
2024-12-03 19:33:41 +0400

    upgrade to 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {PreloadAllModules, provideRouter, withPreloading} from '@angular/router'
 
 import {routes} from './app.routes';
 import {provideClientHydration, withHttpTransferCacheOptions} from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 import {BrowserAnimationsModule, provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {

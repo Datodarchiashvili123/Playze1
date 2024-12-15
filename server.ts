bc102bff357a2d4608081926c8897f680347d88c
Datodarchiashvili123 <datodarchiashvili@gmail.com>
2024-12-15 19:14:39 +0400

    add staging

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import {APP_BASE_HREF} from '@angular/common';
-import {CommonEngine} from '@angular/ssr/node';
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr/node';
 import express from 'express';
-import {fileURLToPath} from 'node:url';
-import {dirname, join, resolve} from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -19,15 +19,9 @@ export function app(): express.Express {
 
     // Example Express Rest API endpoints
     // server.get('/api/**', (req, res) => { });
-
     // Serve static files from /browser
     server.get('*.*', express.static(browserDistFolder, {
-        maxAge: '1y',
-        setHeaders: (res, path) => {
-            if (path.endsWith('robots.txt')) {
-                res.setHeader('Cache-Control', 'public, max-age=0'); // Prevent caching robots.txt
-            }
-        },
+        maxAge: '1y'
     }));
 
     // Serve the Google verification HTML file
@@ -40,20 +34,18 @@ export function app(): express.Express {
         });
     });
 
-    // Serve environment-specific robots.txt
-    const nonIndexableRobotsContent = 'User-agent: *\nDisallow: /';
-    const indexableRobotsContent = 'User-agent: *\nDisallow:';
-    const isProduction = process.env['VERCEL_ENV'] === 'development';
-    console.log(isProduction, ' isProduction')
-
-    server.get('/robots.txt', (req, res) => {
-        res.type('text/plain');
-        res.send(isProduction ? indexableRobotsContent : nonIndexableRobotsContent);
+    server.get('/robots.txt', (req, res, next) => {
+        res.sendFile(join(__dirname, 'dist', 'play', 'assets', 'robots.txt'), (err) => {
+            if (err) {
+                console.error('Error serving robots.txt:', err);
+                next(err);
+            }
+        });
     });
 
     // All regular routes use the Angular engine
     server.get('*', (req, res, next) => {
-        const {protocol, originalUrl, baseUrl, headers} = req;
+        const { protocol, originalUrl, baseUrl, headers } = req;
 
         commonEngine
             .render({
@@ -61,13 +53,10 @@ export function app(): express.Express {
                 documentFilePath: indexHtml,
                 url: `${protocol}://${headers.host}${originalUrl}`,
                 publicPath: browserDistFolder,
-                providers: [{provide: APP_BASE_HREF, useValue: baseUrl}],
+                providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
             })
             .then((html) => res.send(html))
-            .catch((err) => {
-                console.error('Error rendering Angular app:', err);
-                next(err);
-            });
+            .catch((err) => next(err));
     });
 
     return server;
@@ -84,4 +73,3 @@ function run(): void {
 }
 
 run();
-

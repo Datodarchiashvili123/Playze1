33a5fb758b35f16b44cb3db3f8e749b8df58c595
Datodarchiashvili123 <datodarchiashvili@gmail.com>
2025-02-03 01:11:37 +0400

    fix canonicals

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,22 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import {provideRouter} from "@angular/router";
+import {routes} from "./app.routes";
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideClientHydration, withHttpTransferCacheOptions} from "@angular/platform-browser";
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration(
+        withHttpTransferCacheOptions({
+          includePostRequests: false,
+          includeRequestsWithAuthHeaders: false,
+        })
+    ),
   ]
 };
 

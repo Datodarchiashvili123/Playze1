4ffd8b2872cbec692b5616f012b7ee0e50222e59
Datodarchiashvili123 <datodarchiashvili@gmail.com>
2025-02-03 00:15:30 +0400

    fix canonicals

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,19 @@
-export default import('../dist/play/server/server.mjs')
-    .then(module => module.app());
+const { join } = require('path');
+const { existsSync } = require('fs');
+const express = require('express');
+
+const app = express();
+const distFolder = join(__dirname, '../dist/play/browser');
+const server = require('../dist/play/server/main');
+
+if (existsSync(distFolder)) {
+    app.use(express.static(distFolder, {
+        maxAge: '1y'
+    }));
+}
+
+app.use((req, res) => {
+    server.app(req, res);
+});
+
+module.exports = app;
